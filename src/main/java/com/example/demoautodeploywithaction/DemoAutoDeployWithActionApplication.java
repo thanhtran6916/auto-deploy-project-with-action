444cb864eda84492e5df77b55beef56730b8b95e
Tran Van Thanh (OUTSOURCE) <tranvanthanhtran.tt@gmail.com>
2022-07-22 21:36:55 +0700

    init

---
+++ src/main/java/com/example/demoautodeploywithaction/DemoAutoDeployWithActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoautodeploywithaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAutoDeployWithActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAutoDeployWithActionApplication.class, args);
+    }
+
+}
