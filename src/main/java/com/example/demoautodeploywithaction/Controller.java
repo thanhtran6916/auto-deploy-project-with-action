7272a13f98761797b1d620149eddba77922e0f25
Tran Van Thanh (OUTSOURCE) <tranvanthanhtran.tt@gmail.com>
2022-07-28 23:28:35 +0700

    post

--- src/main/java/com/example/demoautodeploywithaction/Controller.java
+++ src/main/java/com/example/demoautodeploywithaction/Controller.java
@@ -3,6 +3,7 @@ package com.example.demoautodeploywithaction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class Controller {
     @GetMapping
     public ResponseEntity<String> demoAction() {
         return new ResponseEntity<>("Hello word", HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postAction() {
+        return new ResponseEntity<>("post action", HttpStatus.OK);
     }
 }
